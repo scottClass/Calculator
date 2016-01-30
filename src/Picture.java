1a27fb948d5210c8390cc0261ee42d8128fa17ae
Scott <Scott@192.168.0.195>
2016-01-29 22:45:44 -0500

    Initial Commit

---
+++ src/Picture.java
@@ -0,0 +1,23 @@
+package Image;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Scott
+ */
+public class Picture {
+    String path;
+    
+    public Picture() {
+        path = "C:\\Users\\Scott\\Downloads\\John Cena.jpg";
+    }
+    
+    public String getImagepath() {
+        return path;
+    }
+}
